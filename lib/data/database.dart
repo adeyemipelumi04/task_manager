24579f8a431a34bf2a185b8e18402b042f9b5acb
Ogwo Chinaza <ogwo.chinaza@yahoo.com>
2025-02-10 14:50:56 +0100

    object cast issue fix

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,19 +1,16 @@
-
 import 'package:hive/hive.dart';
 import 'package:to_do_task/models/task.dart';
 
 class TaskDatabase {
-
-
-  static Future<List<Task>> loadData()async{
-        var taskbox = await Hive.openBox("Taskbox");       
-        var tasks = await taskbox.get("task", defaultValue: []as List<Task>) as List<Task>;
-         print(tasks);
-        return await taskbox.get("task", defaultValue: []as List<Task>) as List<Task>;
-
-  }
-  static void updateDataBase(List<Task> tasks )async{
+  static Future<List<Task>> loadData() async {
     var taskbox = await Hive.openBox("Taskbox");
-    taskbox.put("task", tasks);    
+    var tasks = await taskbox.get("task", defaultValue: <Task>[]);
+    List<Task> taskList = tasks.cast<Task>();
+    return taskList;
+  }
+
+  static void updateDataBase(List<Task> tasks) async {
+    var taskbox = await Hive.openBox("Taskbox");
+    taskbox.put("task", tasks);
   }
 }
